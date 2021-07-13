7190983b5aef906b1a80a2737917038457df30d3
Marcos Chevis Meira <marcoschevism@gmail.com>
2021-07-13 09:37:57 -0300

    Removed Storyboard

--- NotesApp/StartViewController.swift
+++ NotesApp/StartViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        title = "Home"
     }
 
 
