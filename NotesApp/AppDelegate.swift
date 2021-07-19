54f8282341b94efb66c24c40731f9b060139fc60
Marcos Chevis Meira <marcoschevism@gmail.com>
2021-07-19 13:42:19 -0300

    Mystery commit.

--- NotesApp/AppDelegate.swift
+++ NotesApp/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UINavigationController(rootViewController: StartViewController())
+        window.rootViewController = UINavigationController(rootViewController: StartViewController(initialText: ""))
         window.makeKeyAndVisible()
         self.window = window
         
--- NotesApp/StartScreen/ViewControllers/StartViewController.swift
+++ NotesApp/StartScreen/ViewControllers/StartViewController.swift
@@ -11,15 +11,47 @@ class StartViewController: UIViewController {
     
     lazy var textView: UITextView = {
         textView = UITextView(frame: .zero)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .systemBackground
         
         return textView
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
+    init(initialText: String) {
+        super.init(nibName: nil, bundle: nil)
+        textView.text = initialText
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Idea"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Notes", style: .plain, target: self, action: #selector(notesTapped))
+        view.addSubview(textView)
+        
+        setConstraints()
+    }
+    
+    func setConstraints() {
+        
+        let textViewConstraints: [NSLayoutConstraint] = [
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        NSLayoutConstraint.activate(textViewConstraints)
+        
+    }
+    
+    @objc func notesTapped() {
+        
+        navigationController?.pushViewController(NotesListTableViewController(), animated: true)
+    }
     
 }
 
