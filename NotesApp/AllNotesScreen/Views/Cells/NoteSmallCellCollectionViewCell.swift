675416a02c3070c37ef8b135512b46a1db0a28c8
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2022-01-12 13:00:17 -0300

    started cell startup

---
+++ NotesApp/AllNotesScreen/Views/Cells/NoteSmallCellCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  NoteSmallCellCollectionViewCell.swift
+//  NotesApp
+//
+//  Created by Gabriel Ferreira de Carvalho on 12/01/22.
+//
+
+import UIKit
+
+class NoteSmallCellCollectionViewCell: UICollectionViewCell {
+    
+    private lazy var contentTextView: UITextView {
+        
+    }
+    
+    override func prepareForReuse() {
+        
+    }
+    
+    func setup() {
+        
+    }
+    
+    func setupConstraint() {
+        
+    }
+}
