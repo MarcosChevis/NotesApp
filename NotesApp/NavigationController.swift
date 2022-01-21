efdcb3a146c09c2fea2c4b7be284e722895c6a34
caroltaus <carolinetaus@hotmail.com>
2022-01-21 11:16:49 -0300

    rebecca taus

--- NotesApp/NavigationController.swift
+++ NotesApp/NavigationController.swift
@@ -10,13 +10,10 @@ import UIKit
 
 class NavigationController : UINavigationController {
     
-    override var preferredStatusBarStyle : UIStatusBarStyle {
-        
+    override var preferredStatusBarStyle : UIStatusBarStyle {       
         if let topVC = viewControllers.last {
-             //return the status property of each VC, look at step 2
              return topVC.preferredStatusBarStyle
          }
-        
         return .default
     }
 }
