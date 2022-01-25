e14872c384a83d261110f920fc591128a5ed2b6c
caroltaus <carolinetaus@hotmail.com>
2022-01-25 11:35:06 -0300

    Theme tests

--- NotesAppTests/ViewControllers/ThemesViewControllerTest.swift
+++ NotesAppTests/ViewControllers/ThemesViewControllerTest.swift
@@ -23,7 +23,7 @@ class ThemesViewControllerTest: XCTestCase {
         notificationServiceDummy = .init()
         localStorageServiceDummy = .init()
         settings = .init(localStorageService: localStorageServiceDummy, notificationService: notificationServiceDummy)
-        sut = ThemesViewController(palette: colorSet, collectionDataSource: ThemesCollectionDataSouce(), notificationService: notificationServiceDummy, settings: settings)
+        sut = ThemesViewController(palette: .classic, collectionDataSource: ThemesCollectionDataSouce(), notificationService: notificationServiceDummy, settings: settings)
         sut.coordinator = coordinatorDummy
         _ = sut.view
     }
@@ -33,6 +33,33 @@ class ThemesViewControllerTest: XCTestCase {
     }
     
     func testDidSelectTheme() {
-        sut.collectionDataSource(
+        selectTheme(at: 1)
+        sut.collectionDataSource.data.forEach { theme in
+            if theme.colorSet == .neon {
+                XCTAssertTrue(theme.isSelected)
+            }
+            else {
+                XCTAssertFalse(theme.isSelected)
+            }
+        }
     }
+    
+    func testDidChangeTheme() {
+        XCTAssertEqual(.classic, settings.theme)
+        selectTheme(at: 2)
+        XCTAssertEqual(.christmas, settings.theme)
+    }
+    
+    func testDidChangePalette() {
+        XCTAssertEqual("didChangeTheme:", notificationServiceDummy.observers.first!)
+        XCTAssertEqual([], notificationServiceDummy.postedNotification)
+        selectTheme(at: 2)
+        XCTAssertEqual(.init("ThemeManager_DidChangeTheme"), notificationServiceDummy.postedNotification.first!)
+        XCTAssertEqual(.christmas, notificationServiceDummy.postedObjects.first! as! ColorSet)
+    }
+    
+    func selectTheme(at index: Int) {
+        sut.collectionView(sut.contentView.collectionView, didSelectItemAt: IndexPath(row: index, section: 0))
+    }
+    
 }
